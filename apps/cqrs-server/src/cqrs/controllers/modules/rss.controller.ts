14a31d087bb98c114a03da4a0132207b849dcd15
Branlii <guillot.raphael11@gmail.com>
2026-03-06 17:09:49 +0100

    feat(cqrs-server): add missing tldr endpoints

--- apps/cqrs-server/src/cqrs/controllers/modules/rss.controller.ts
+++ apps/cqrs-server/src/cqrs/controllers/modules/rss.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Sse } from "@nestjs/common";
 import { QueryBus } from "@nestjs/cqrs";
 import { from, interval, type Observable, startWith, switchMap } from "rxjs";
+import { GetRssDevopsQuery } from "src/cqrs/queries/definitions/modules/rss/devops/get-rss-devops.query";
+import { GetRssFoundersQuery } from "src/cqrs/queries/definitions/modules/rss/founders/get-rss-founders.query";
+import { GetRssInfosecQuery } from "src/cqrs/queries/definitions/modules/rss/infosec/get-rss-infosec.query";
+import { GetRssMarketingQuery } from "src/cqrs/queries/definitions/modules/rss/marketing/get-rss-marketing.query";
+import { GetRssProductQuery } from "src/cqrs/queries/definitions/modules/rss/product/get-rss-product.query";
 import { GetRssTechQuery } from "src/cqrs/queries/definitions/modules/rss/tech/get-rss-tech.query";
+import { GetRssWebdevQuery } from "src/cqrs/queries/definitions/modules/rss/webdev/get-rss-webdev.query";
 
 @Controller("rss")
 export class RssController {
@@ -12,12 +18,125 @@ export class RssController {
 		const intervalTime =
 			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
 
-		// The interval will force refresh to ensure fresh data at regular intervals
+		return interval(intervalTime * 60 * 1000).pipe(
+			startWith(0),
+			switchMap((tick) =>
+				from(this.queryBus.execute(new GetRssTechQuery("tech", tick > 0))).pipe(
+					switchMap((result) =>
+						Promise.resolve({ data: result } as MessageEvent),
+					),
+				),
+			),
+		);
+	}
+
+	@Sse("webdev")
+	getWebdevRss(): Observable<MessageEvent> {
+		const intervalTime =
+			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
+
 		return interval(intervalTime * 60 * 1000).pipe(
 			startWith(0),
 			switchMap((tick) =>
 				from(
-					this.queryBus.execute(new GetRssTechQuery("webdev", tick > 0)),
+					this.queryBus.execute(new GetRssWebdevQuery("webdev", tick > 0)),
+				).pipe(
+					switchMap((result) =>
+						Promise.resolve({ data: result } as MessageEvent),
+					),
+				),
+			),
+		);
+	}
+
+	@Sse("infosec")
+	getInfosecRss(): Observable<MessageEvent> {
+		const intervalTime =
+			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
+
+		return interval(intervalTime * 60 * 1000).pipe(
+			startWith(0),
+			switchMap((tick) =>
+				from(
+					this.queryBus.execute(new GetRssInfosecQuery("infosec", tick > 0)),
+				).pipe(
+					switchMap((result) =>
+						Promise.resolve({ data: result } as MessageEvent),
+					),
+				),
+			),
+		);
+	}
+
+	@Sse("marketing")
+	getMarketingRss(): Observable<MessageEvent> {
+		const intervalTime =
+			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
+
+		return interval(intervalTime * 60 * 1000).pipe(
+			startWith(0),
+			switchMap((tick) =>
+				from(
+					this.queryBus.execute(
+						new GetRssMarketingQuery("marketing", tick > 0),
+					),
+				).pipe(
+					switchMap((result) =>
+						Promise.resolve({ data: result } as MessageEvent),
+					),
+				),
+			),
+		);
+	}
+
+	@Sse("product")
+	getProductRss(): Observable<MessageEvent> {
+		const intervalTime =
+			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
+
+		return interval(intervalTime * 60 * 1000).pipe(
+			startWith(0),
+			switchMap((tick) =>
+				from(
+					this.queryBus.execute(new GetRssProductQuery("product", tick > 0)),
+				).pipe(
+					switchMap((result) =>
+						Promise.resolve({ data: result } as MessageEvent),
+					),
+				),
+			),
+		);
+	}
+
+	@Sse("founders")
+	getFoundersRss(): Observable<MessageEvent> {
+		const intervalTime =
+			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
+
+		return interval(intervalTime * 60 * 1000).pipe(
+			startWith(0),
+			switchMap((tick) =>
+				from(
+					this.queryBus.execute(new GetRssFoundersQuery("founders", tick > 0)),
+				).pipe(
+					switchMap((result) =>
+						Promise.resolve({ data: result } as MessageEvent),
+					),
+				),
+			),
+		);
+	}
+
+	@Sse("devops")
+	getDevopsRss(): Observable<MessageEvent> {
+		const intervalTime =
+			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
+
+		return interval(intervalTime * 60 * 1000).pipe(
+			startWith(0),
+			switchMap((tick) =>
+				from(
+					this.queryBus.execute(new GetRssDevopsQuery("devops", tick > 0)),
 				).pipe(
 					switchMap((result) =>
 						Promise.resolve({ data: result } as MessageEvent),
--- apps/cqrs-server/src/cqrs/queries/handlers/modules/rss/founders/get-founders.rss.ts
+++ apps/cqrs-server/src/cqrs/queries/handlers/modules/rss/founders/get-founders.rss.ts
@@ -1,20 +1,22 @@
 import { type IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { RedisService } from "src/cache/redis";
-import { GetRssTechQuery } from "src/cqrs/queries/definitions/modules/rss/tech/get-rss-tech.query";
+import { GetRssFoundersQuery } from "src/cqrs/queries/definitions/modules/rss/founders/get-rss-founders.query";
 import { TldrRssParserService } from "../../../../../../rss/parser/tldr/tldr-rss-parser.service";
 import type { IParsedRss } from "../../../../interfaces";
 
-@QueryHandler(GetRssTechQuery)
-export class GetRssTechHandler implements IQueryHandler<GetRssTechQuery> {
+@QueryHandler(GetRssFoundersQuery)
+export class GetRssFoundersHandler
+	implements IQueryHandler<GetRssFoundersQuery>
+{
 	constructor(
 		private readonly rssParserService: TldrRssParserService,
 		private readonly redisService: RedisService,
 	) {}
 
-	async execute(query: GetRssTechQuery): Promise<IParsedRss[]> {
+	async execute(query: GetRssFoundersQuery): Promise<IParsedRss[]> {
 		const intervalTime =
 			Number(process.env.RSS_FETCH_INTERVAL_IN_MINUTES) || 60;
-		const redisKey = `rss:tech:${query.path || "tech"}`;
+		const redisKey = `rss:founders:${query.path || "founders"}`;
 		const ttlSeconds = intervalTime * 60;
 
 		// If force refresh, skip cache and fetch new data
@@ -37,7 +39,7 @@ export class GetRssTechHandler implements IQueryHandler<GetRssTechQuery> {
 	}
 
 	private async fetchRssFromApi(path?: string): Promise<IParsedRss[]> {
-		const rssPath = path || "tech";
+		const rssPath = path || "founders";
 		const url = new URL(`rss/${rssPath}`, process.env.TLDR_API_URL);
 		return await this.rssParserService.getNewestItems(url.toString());
 	}
